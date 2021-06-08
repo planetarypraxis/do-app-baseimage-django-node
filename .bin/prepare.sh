8ce5408d1f2eef4c50bc63985573bd97523449ee
Chris Devereux <chris.devereux@pm.me>
2021-06-08 15:57:16 +0100

    adds textract dependencies

--- .bin/prepare.sh
+++ .bin/prepare.sh
@@ -17,6 +17,9 @@ apt-get install --yes --quiet --no-install-recommends \
     libboost-python-dev \
     libtiff5-dev libopenjp2-7-dev zlib1g-dev \
     libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
-    libharfbuzz-dev libfribidi-dev libxcb1-dev
+    libharfbuzz-dev libfribidi-dev libxcb1-dev \
+    python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils tesseract-ocr \
+    flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev
+
 
 npm i -g yarn
